a54935b1233d8a47410862329dd57923f5c2a896
eli56 <elianatorres56@gmail.com>
2022-12-13 19:42:23 -0500

    Mi primer cambio

---
+++ src/main/java/co/com/empresamao/areait/advantage/utils/Constants.java
@@ -0,0 +1,6 @@
+package co.com.empresamao.areait.advantage.utils;
+
+public class Constants {
+    public  static final String URL="https://www.advantageonlineshopping.com/#/";
+    public static final String ACTOR_PAGE= "Mao";
+}
